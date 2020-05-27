b61cd01af1b48630343e7b2e2afb3f1a3dd9ca40
Arne Groskurth <arne.groskurth@gears9.de>
2020-05-27 19:52:49 +0200

    Added support for order-sensitive expectation

--- pyfcopy_test/tree_progress_listener_tester.py
+++ pyfcopy_test/tree_progress_listener_tester.py
@@ -1,37 +1,60 @@
 
-from typing import Set, Optional, List
+from pathlib import Path
+from typing import Set, Optional, List, Union
 
 from pyfcopy import TreeProgressListener
 
 
 class TreeProgressListenerTester(TreeProgressListener):
 
-    def __init__(self, expected_paths: Optional[Set[str]] = None):
+    def __init__(self, expected_paths: Optional[Union[Set[str], List[str]]] = None):
 
         self._expected_paths = expected_paths
 
-        self.currently_running = False
+        self.current_step_index = None
         self.current_relative_path = None
 
-    def begin(self, relative_paths: List[str]) -> None:
+    def begin(self, relative_paths: List[Path]) -> None:
 
-        assert self.currently_running is False
-        assert self._expected_paths is None or set(relative_paths) == self._expected_paths
+        relative_paths = [str(path) for path in relative_paths]
 
-        self.currently_running = True
+        assert self.current_step_index is None
 
-    def next(self, relative_path: str) -> None:
+        if type(self._expected_paths) is list:
 
-        assert self.currently_running
+            assert self._expected_paths == relative_paths
+
+        elif type(self._expected_paths) is set:
+
+            assert self._expected_paths == set(relative_paths)
+
+        else:
+
+            assert self._expected_paths is None
+
+        self.current_step_index = 0
+
+    def next(self, relative_path: Path) -> None:
+
+        relative_path = str(relative_path)
+
+        assert self.current_step_index is not None
         assert self._expected_paths is None or relative_path in self._expected_paths
+
+        if type(self._expected_paths) is list:
+
+            assert self.current_step_index < len(self._expected_paths)
+            assert self._expected_paths[self.current_step_index] == relative_path
+
+        self.current_step_index += 1
 
     def finish(self) -> None:
 
-        assert self.currently_running
+        assert self.current_step_index is not None
 
-        self.currently_running = False
+        self.current_step_index = None
         self.current_relative_path = None
 
     def assert_consistent_run(self):
 
-        assert self.currently_running is False
+        assert self.current_step_index is None
