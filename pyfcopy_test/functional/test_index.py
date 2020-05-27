bffaa92160cbd43aecd1b3d64c614995ac93c0fd
Arne Groskurth <arne.groskurth@gears9.de>
2020-05-27 22:02:48 +0200

    Wording

--- pyfcopy_test/functional/test_index.py
+++ pyfcopy_test/functional/test_index.py
@@ -4,7 +4,7 @@ from pathlib import Path
 import pytest
 
 from pyfcopy.index import index, Order
-from pyfcopy_test.tree_list_assertions import assert_same_tree_list
+from pyfcopy_test.index_assertions import assert_same_index
 
 
 def test_invalid_path(tmp_path: Path):
@@ -18,7 +18,7 @@ def test_empty(tmp_path: Path):
 
     paths = index(tmp_path)
 
-    assert_same_tree_list(paths, ["."])
+    assert_same_index(paths, ["."])
 
 
 def test_circular_symlink(tmp_path: Path):
@@ -27,19 +27,19 @@ def test_circular_symlink(tmp_path: Path):
 
     paths = index(tmp_path)
 
-    assert_same_tree_list(paths, [".", "a-symlink"])
+    assert_same_index(paths, [".", "a-symlink"])
 
 
 def test_file_path(tmp_path: Path):
 
     (tmp_path / "file.ext").touch()
 
-    assert_same_tree_list(
+    assert_same_index(
         index(tmp_path / "file.ext", absolute=True),
         [tmp_path / "file.ext"]
     )
 
-    assert_same_tree_list(
+    assert_same_index(
         index(tmp_path / "file.ext", absolute=False),
         ["."]
     )
@@ -56,7 +56,7 @@ def test_simple(tmp_path: Path):
 
     paths = index(tmp_path, order=Order.PRE)
 
-    assert_same_tree_list(paths, [
+    assert_same_index(paths, [
         ".",
         'dir1',
         'dir1/file2',
@@ -73,14 +73,14 @@ def test_order(tmp_path: Path):
     (tmp_path / "dir1/dir2").mkdir()
     (tmp_path / "dir1/dir2/file").touch()
 
-    assert_same_tree_list(index(tmp_path, order=Order.PRE), [
+    assert_same_index(index(tmp_path, order=Order.PRE), [
         ".",
         "dir1",
         "dir1/dir2",
         "dir1/dir2/file",
     ])
 
-    assert_same_tree_list(index(tmp_path, order=Order.POST), [
+    assert_same_index(index(tmp_path, order=Order.POST), [
         "dir1/dir2/file",
         "dir1/dir2",
         "dir1",
@@ -92,12 +92,12 @@ def test_absolute_path(tmp_path: Path):
 
     (tmp_path / "file.ext").touch()
 
-    assert_same_tree_list(index(tmp_path, absolute=False), [
+    assert_same_index(index(tmp_path, absolute=False), [
         ".",
         "file.ext",
     ])
 
-    assert_same_tree_list(index(tmp_path, absolute=True), [
+    assert_same_index(index(tmp_path, absolute=True), [
         tmp_path,
         tmp_path / "file.ext",
     ])
@@ -107,5 +107,5 @@ def test_root_exclusion(tmp_path: Path):
 
     (tmp_path / "some-dir").mkdir()
 
-    assert_same_tree_list(index(tmp_path / "some-dir", include_root=False), [])
-    assert_same_tree_list(index(tmp_path / "some-dir", include_root=True), ["."])
+    assert_same_index(index(tmp_path / "some-dir", include_root=False), [])
+    assert_same_index(index(tmp_path / "some-dir", include_root=True), ["."])
--- pyfcopy_test/index_assertions.py
+++ pyfcopy_test/index_assertions.py
@@ -3,7 +3,7 @@ from pathlib import Path
 from typing import List, Union
 
 
-def assert_same_tree_list(actual_index: List[Path], expected_index: Union[List[Path], List[str]]) -> None:
+def assert_same_index(actual_index: List[Path], expected_index: Union[List[Path], List[str]]) -> None:
 
     actual_index = [str(path) for path in actual_index]
     expected_index = [str(path) for path in expected_index]
