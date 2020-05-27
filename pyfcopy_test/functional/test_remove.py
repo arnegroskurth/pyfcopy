9611de632417bb3c34cbfe935dc0d77ccb7e2548
Arne Groskurth <arne.groskurth@gears9.de>
2020-05-27 23:09:07 +0200

    Refactored away tree fixture

--- pyfcopy_test/functional/test_remove.py
+++ pyfcopy_test/functional/test_remove.py
@@ -4,7 +4,6 @@ from pathlib import Path
 import pytest
 
 from pyfcopy.remove import remove
-from pyfcopy_test.tree_fixture import prepare_tree
 from pyfcopy_test.tree_progress_listener_tester import TreeProgressListenerTester
 
 
@@ -39,33 +38,23 @@ def test_remove_dir(tmp_path: Path):
 
 def test_remove_tree(tmp_path: Path):
 
-    prepare_tree(
-        tmp_path,
-        directory_map={
-            "tree": 0o777,
-            "tree/empty": 0o777,
-            "tree/sub1": 0o775,
-            "tree/sub2": 0o755,
-            "tree/sub2/sub21": 0o757,
-        },
-        file_map={
-            "tree/a.ext": 0o777,
-            "tree/sub1/b.ext": 0o775,
-            "tree/sub2/sub21/c.ext": 0o757,
-        }
-    )
+    (tmp_path / "dir").mkdir()
+    (tmp_path / "dir/a.ext").touch()
+    (tmp_path / "dir/empty").mkdir()
+    (tmp_path / "dir/sub1").mkdir()
+    (tmp_path / "dir/sub1/b.ext").mkdir()
+    (tmp_path / "dir/sub2").mkdir()
+    (tmp_path / "dir/sub2/sub21").mkdir()
+    (tmp_path / "dir/sub2/sub21/c.ext").touch()
 
-    path = tmp_path / "tree"
+    path = tmp_path / "dir"
 
     tree_progress_listener = TreeProgressListenerTester({
         ".", "empty", "sub1", "sub2", "sub2/sub21",
         "a.ext", "sub1/b.ext", "sub2/sub21/c.ext",
     })
 
-    remove(
-        path,
-        tree_progress_listener=tree_progress_listener
-    )
+    remove(path, tree_progress_listener=tree_progress_listener)
 
     assert not path.exists()
 
